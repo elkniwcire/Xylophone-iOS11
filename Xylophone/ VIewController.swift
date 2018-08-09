17fc6524b7102b609d601c55a29e4f6aa84d480f
Eric <elkniwcire@gmail.com>
2018-08-08 23:11:51 -0400

    - added sound actions

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController{
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    var sounds : [String] = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        let soundPressed = sounds[sender.tag - 1]
         
+        //credit to Matt Gibson on stack overflow
+        if let soundURL = Bundle.main.url(forResource: soundPressed, withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound);
+        }
         
     }
     
